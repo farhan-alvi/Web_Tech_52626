95eaa69a76024e2c0cebbdc3a2f610cc40bef0c9
Farhan Alvi <itsfarhanalvi315@gmail.com>
2026-05-08 11:32:41 +0600

    Final Lab Task 2

---
+++ AJAX_JSON_Task_1/student.php
@@ -0,0 +1,17 @@
+<?php
+
+// Student data array
+
+$student = array(
+
+    "name" => "Sk. Farhan Alvi",
+    "id" => "23-52626-2",
+    "department" => "CSE",
+    "cgpa" => "3.85"
+
+);
+
+
+echo json_encode($student);
+
+?>
